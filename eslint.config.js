822b78541b3dd8e7fd023de4a1ffad274bbc5122
Marko Smoljan <markosmoljan@Markos-MacBook-Pro.local>
2024-11-27 12:44:16 +0100

    vite and lint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended, 
+      ...tseslint.configs.recommended
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
+    plugins: {      
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -23,6 +26,11 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "semi": "warn",
+      "max-len": ["warn", { "code": 100 }],
+      "no-console": "warn"
     },
   },
 )
